ecb51d58321cf309f909fe8e867235de68f3fafb
jgstratton <jgstratt@sdf.org>
2018-11-24 15:04:42 -0600

    Initial add

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+import re
+from . import brion_pysin_lib
 
 class CutUp(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('up.cut.intent')
-    def handle_up_cut(self, message):
-        self.speak_dialog('up.cut')
-
+    @intent_handler(IntentBuilder("").require("CutUp").require("Words"))
+    def handle_pysin_brion(self, message):
+        utterance = message.data.get('utterance')
+        repeat = re.sub('^.*?' + message.data['CutUp'], '', utterance)
+        cutup = brion_pysin_lib.traditional_cutup( repeat, 'word', 1, 1, 100 )
+        self.speak(cutup.strip())
 
 def create_skill():
     return CutUp()
